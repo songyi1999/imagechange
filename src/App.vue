0d1e5578dd90cbbe277edcc33efebeaee8791cc3
songyi1999 <452350370@qq.com>
2023-09-10 21:16:44 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+   
+    <HelloWorld :images="images" />
   </div>
 </template>
 
@@ -12,17 +12,22 @@ export default {
   name: 'App',
   components: {
     HelloWorld
-  }
+  },
+  data(){
+    return{
+      images: (() => {
+        let imagePaths = [];
+        for (let i = 1; i <= 10; i++) {
+          imagePaths.push(`images/${i}.jpg`);
+        }
+        return imagePaths;
+      })(),
+    }
+  },
+  
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,144 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div id="slider" ref="slides">
+    <div class="slide" v-for="(img, k) in images" :key="k">
+      <img :src="img" />
+    </div>
   </div>
 </template>
 
 <script>
+import { gsap, Power2 } from "gsap";
+
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
-    msg: String
-  }
-}
+    images: Array,
+  },
+  data() {
+    return {
+      slides: [],
+    };
+  },
+  created() {
+    this.randomPosition();
+  },
+  mounted() {
+    let app = this;
+    this.$nextTick(() => {
+      app.slides = Array.from(this.$refs.slides.children);
+      
+      // 随机放置第2到最后一张图片的位置在上下左右左上左下右上右下8个位置
+      app.randomPosition();
+      function foo() {
+        let tl = gsap.timeline({
+          onComplete: () => {
+            console.log("播放完成，重新设置位置");
+            // 重置位置
+            app.randomPosition();
+            console.log("幻灯片位置", app.slides[0]);
+            setTimeout(() => {
+              foo();
+            }, 1000);
+          },
+        });
+        tl.to(app.slides[0], {
+          duration: 3,
+          x: 0,
+          y: 0,
+          ease: Power2.easeOut,
+        });
+        for (let i = 1; i < app.slides.length; i++) { 
+                   tl.to(app.slides[i], { 
+            duration: 3,
+            x: 0,
+            y: 0,
+            ease: Power2.easeOut,
+          });
+        }
+      }
+      foo();
+    });
+  },
+  methods: {
+    randomPosition() {
+      let app = this;
+      let screenwidth = window.innerWidth;
+      let screenheight = window.innerHeight;
+      let left = {
+        x: -screenwidth,
+        y: 0,
+      };
+      let right = {
+        x: screenwidth,
+        y: 0,
+      };
+      let top = {
+        x: 0,
+        y: -screenheight,
+      };
+      let bottom = {
+        x: 0,
+        y: screenheight,
+      };
+      let topleft = {
+        x: -screenwidth,
+        y: -screenheight,
+      };
+      let topright = {
+        x: screenwidth,
+        y: -screenheight,
+      };
+      let bottomleft = {
+        x: -screenwidth,
+        y: screenheight,
+      };
+      let bottomright = {
+        x: screenwidth,
+        y: screenheight,
+      };
+      let position = [
+        left,
+        right,
+        top,
+        bottom,
+        topleft,
+        topright,
+        bottomleft,
+        bottomright,
+      ];
+
+      for (let i = 1; i < app.slides.length; i++) {
+        let random = Math.floor(Math.random() * position.length);
+        // console.log("位置",random);
+        console.log("位置", position[random].x, position[random].y);
+        gsap.set(app.slides[i], {
+          x: position[random].x,
+          y: position[random].y,
+        });
+        console.log("幻灯片位置", this.slides[i].style.transform);
+      }
+    },
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+#slider {
+  position: relative;
+  width: 100%;
+  height: 100vh;
+  overflow: hidden;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.slide {
+  position: absolute;
+  width: 100%;
+  height: 100%;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+
+img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
 }
 </style>
